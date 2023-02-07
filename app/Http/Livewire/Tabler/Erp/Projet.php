4d8cc58da01366e41a59df57f6635d1471507b63
Desmond8500 <desmond8500@gmail.com>
2023-02-07 22:26:03 +0000

    project update

--- app/Http/Livewire/Tabler/Erp/Projet.php
+++ app/Http/Livewire/Tabler/Erp/Projet.php
@@ -2,20 +2,15 @@
 
 namespace App\Http\Livewire\Tabler\Erp;
 
-use App\Models\Invoice;
-use App\Models\Projet as ModelsProjet;
 use Livewire\Component;
+use phpDocumentor\Reflection\Types\This;
 
 class Projet extends Component
 {
-    public $projet;
-    public $tab = 1;
+    public $projet_id;
 
-    public $breadcrumbs;
-
-    public function mount($projet_id)
-    {
-        $this->projet = ModelsProjet::find($projet_id);
+    public function mount($projet_id){
+        $this->projet_id = $projet_id;
 
         $this->breadcrumbs = array(
             array('name' => 'Clients', 'route' => route('tabler.clients')),
@@ -27,14 +22,7 @@ class Projet extends Component
     public function render()
     {
         return view('livewire.tabler.erp.projet',[
-            'projet' => $this->projet,
-            // 'client' => $this->projet->client->name,
-            'devis' => Invoice::where('projet_id', $this->projet->id)->get(),
+            'projet_id' => $this->projet_id,
         ])->extends('app.layout')->section('content');
-    }
-
-    public function selectTab(int $tab)
-    {
-        $this->tab = $tab;
     }
 }
