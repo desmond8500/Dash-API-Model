60e593c14f6fad2cb9bd0f462a60af199ff7f892
kratos <desmond8500@gmail.com>
2023-02-10 13:52:47 +0000

    fix

--- resources/views/livewire/tabler/erp/building-card.blade.php
+++ resources/views/livewire/tabler/erp/building-card.blade.php
@@ -43,7 +43,7 @@
             <div class="modal-content">
                 <form>
                     <div class="modal-header">
-                        <h5 class="modal-title">Ajouter un local {{ $stage_id }}</h5>
+                        <h5 class="modal-title">Ajouter un local </h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
