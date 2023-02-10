8827b2388f00f53e40769cb36ef836b3758f78fe
kratos <desmond8500@gmail.com>
2023-02-10 11:25:13 +0000

    update

--- resources/views/livewire/tabler/task/task-projet-list.blade.php
+++ resources/views/livewire/tabler/task/task-projet-list.blade.php
@@ -1,3 +1,20 @@
-<div>
-    {{-- Be like water. --}}
+ <div class="row">
+    <div class="col-md-4">
+        <div class="card">
+            <div class="card-header">
+                <div class="card-title">Liste des taches</div>
+            </div>
+            <div class="card-body">
+                @foreach ($taches as $tache)
+                    @livewire('tabler.task.task-card', ['tache' => $tache], key($tache->id))
+                    <div wire:click="getTask('{{ $tache->id }}')" class="text-end" type>Consulter</div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+    <div class="col-md-8">
+        @if ($task)
+            @dump($task)
+        @endif
+    </div>
 </div>
