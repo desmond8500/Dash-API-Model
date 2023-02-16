ca711f54b08ec7428246f4506294572ca79c57a4
Desmond8500 <desmond8500@gmail.com>
2023-02-16 12:37:32 +0000

    4lightgallery intégration

--- resources/views/app/layout.blade.php
+++ resources/views/app/layout.blade.php
@@ -10,6 +10,9 @@
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/atom-one-dark.min.css">
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta16/dist/css/tabler-vendors.min.css">
+
+    <script src=" https://cdn.jsdelivr.net/npm/lightgallery@2.7.1/lightgallery.min.js "></script>
+    <link href=" https://cdn.jsdelivr.net/npm/lightgallery@2.7.1/css/lightgallery.min.css " rel="stylesheet">
     @livewireStyles
 </head>
 
