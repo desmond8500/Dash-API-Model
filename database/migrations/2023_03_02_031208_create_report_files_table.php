c0e5fa79b1dab350a6b4c931912d6f2b890938a7
kratos <desmond8500@gmail.com>
2023-03-27 18:57:08 +0000

    de

--- database/migrations/2023_03_02_031208_create_report_files_table.php
+++ database/migrations/2023_03_02_031208_create_report_files_table.php
@@ -10,7 +10,7 @@ class CreateReportFilesTable extends Migration
     {
         Schema::create('report_files', function (Blueprint $table) {
             $table->increments('id');
-            $table->foreignId('report_id')
+            $table->foreignId('report_id');
             $table->string('name');
             $table->string('folder');
             $table->string('extension');
