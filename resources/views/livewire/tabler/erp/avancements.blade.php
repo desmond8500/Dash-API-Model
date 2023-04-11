88b1ae43418a86e417f0487c0497b39519c2d882
kratos <desmond8500@gmail.com>
2023-04-11 02:33:57 +0000

    generer

--- resources/views/livewire/tabler/erp/avancements.blade.php
+++ resources/views/livewire/tabler/erp/avancements.blade.php
@@ -131,6 +131,9 @@
                                                         <button class="btn btn-secondary btn-icon" data-bs-toggle="modal" data-bs-target="#addRow" wire:click="$set('avancement_row_id','{{ $section->id }}')">
                                                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"></path> <path d="M12 5l0 14"></path> <path d="M5 12l14 0"></path> </svg>
                                                         </button>
+                                                        <a class="btn btn-icon btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#generateRow" wire:click="$set('avancement_row_id','{{ $section->id }}')">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"></path> <path d="M12 5l0 14"></path> <path d="M5 12l14 0"></path> </svg>
+                                                        </a>
                                                     </td>
                                                 @endif
                                             </tr>
@@ -210,4 +213,12 @@
         'method' => "add_category"
     ])
     <script> window.addEventListener('close-modal', event => { $("#addCategorie").modal('hide'); }) </script>
+
+    @include('_tabler.modal',[
+        'id' => "generateRow",
+        'title' => "Générer une ligne",
+        'include' => "_tabler.erp.generate_row_form",
+        'method' => "generate_rows"
+    ])
+    <script> window.addEventListener('close-modal', event => { $("#generateRow").modal('hide'); }) </script>
 </div>
