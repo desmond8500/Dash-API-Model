c6e5692df85ba0ee39c61c2ebf0709443c3d8a7d
kratos <desmond8500@gmail.com>
2023-05-21 22:41:36 +0000

    fix

--- resources/views/livewire/tabler/task/task-projet-list.blade.php
+++ resources/views/livewire/tabler/task/task-projet-list.blade.php
@@ -1,35 +1,27 @@
- <div class="row">
-    <div class="col-md-6">
-        sdfsdf
-        @if ($task)
-            @dump($task)
-        @endif
-    </div>
-    <div class="col-md-6">
-        <div class="card">
-            <div class="card-header">
-                <div class="card-title">Liste des taches</div>
-                <div class="card-actions">
-                    @livewire('tabler.task.task-add', ['room_id' => 0, 'projet_id'=> $projet_id])
-                </div>
+ <div >
+    <div class="card">
+        <div class="card-header">
+            <div class="card-title">Liste des taches</div>
+            <div class="card-actions">
+                @livewire('tabler.task.task-add', ['room_id' => 0, 'projet_id'=> $projet_id])
             </div>
-            <div class="card-body">
-                @foreach ($taches as $tache)
-                    @livewire('tabler.task.task-card', ['tache' => $tache], key($tache->id))
-                @endforeach
-                <div class="mt-3 d-flex justify-content-between">
-                    <div>
-                        {{ $taches->links() }}
-                    </div>
-                    <div>
-                        <button class="btn btn-primary" wire:click="$toggle('task_toggle')">
-                            @if ($this->task_toggle)
-                                Taches Terminés
-                            @else
-                                Taches en Cours
-                            @endif
-                        </button>
-                    </div>
+        </div>
+        <div class="card-body">
+            @foreach ($taches as $tache)
+                @livewire('tabler.task.task-card', ['tache' => $tache], key($tache->id))
+            @endforeach
+            <div class="mt-3 d-flex justify-content-between">
+                <div>
+                    {{ $taches->links() }}
+                </div>
+                <div>
+                    <button class="btn btn-primary" wire:click="$toggle('task_toggle')">
+                        @if ($this->task_toggle)
+                            Taches Terminés
+                        @else
+                            Taches en Cours
+                        @endif
+                    </button>
                 </div>
             </div>
         </div>
